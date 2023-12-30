1df992cca1151ee3e17419fc37915ca356563653
Alejandro Haibi <aehaibi@gmail.com>
2023-12-30 00:18:16 -0800

    data persistence between sessions

---
+++ Assets/Scripts/MainManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    public static MainManager Instance;
+
+    public Color TeamColor;
+
+    private void Awake()
+    {
+        //this pattern is called a "singleton". Use it to ensure that only a single instance of the MainManager
+        //can ever exist, acting as a central point of access
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        //you can call MainManager.Instance from any other script, no ref needed like when assigning
+        //a GameObject to script properties in the Inspector
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        LoadColor();
+    }
+
+    [System.Serializable]
+    class SaveData 
+    {
+        public Color TeamColor;
+    }
+
+    public void SaveColor()
+    {
+        SaveData data = new SaveData();
+        data.TeamColor = TeamColor;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    //this method is a reversal of the SaveColor method
+    public void LoadColor() 
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if(File.Exists(path)) 
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            TeamColor = data.TeamColor;
+        }
+    }
+}
